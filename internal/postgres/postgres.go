afdf51e3fe9b9f72de1f4d8ce1f6ecae23588f73
Amin Abdi <aminabdi@Amins-MacBook-Air.local>
2025-02-22 19:32:42 +0000

    Add update ISA function

--- internal/postgres/postgres.go
+++ internal/postgres/postgres.go
@@ -122,3 +122,51 @@ func (s *Store) GetIsa(ctx context.Context, id string) (*ISA, error) {
 
 	return &isa, nil
 }
+
+func (s *Store) UpdateIsa(ctx context.Context, isa ISA) (*ISA, error) {
+	logger := logrus.New().WithContext(ctx)
+	now := time.Now()
+	logger = logger.WithFields(logrus.Fields{
+		"isa_id":            isa.ID,
+		"user_id":           isa.UserID,
+		"fund_ids":          isa.FundIDs,
+		"cash_balance":      isa.CashBalance,
+		"investment_amount": isa.InvestmentAmount,
+	})
+
+	query := `UPDATE isas
+	SET fund_ids = $1, cash_balance = $2, investment_amount = $3, updated_at = $4
+	WHERE id = $5
+	RETURNING id, user_id, fund_ids, cash_balance, investment_amount, created_at, updated_at`
+
+	args := []any{
+		pq.Array(isa.FundIDs),
+		isa.CashBalance,
+		isa.InvestmentAmount,
+		now, // Update the timestamp
+		isa.ID,
+	}
+
+	var updatedISA ISA
+	err := s.db.QueryRow(ctx, query, args...).Scan(
+		&updatedISA.ID,
+		&updatedISA.UserID,
+		&updatedISA.FundIDs,
+		&updatedISA.CashBalance,
+		&updatedISA.InvestmentAmount,
+		&updatedISA.CreatedAt,
+		&updatedISA.UpdatedAt,
+	)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			logger.WithError(err).Error("ISA not found for update")
+			return nil, fmt.Errorf("isa not found: %w", err)
+		}
+		logger.WithError(err).Error("Failed to execute update isa query")
+		return nil, fmt.Errorf("failed to execute update isa query: %w", err)
+	}
+
+	logger.Info("ISA successfully updated")
+	return &updatedISA, nil
+}
--- internal/postgres/postgres_test.go
+++ internal/postgres/postgres_test.go
@@ -15,8 +15,9 @@ import (
 )
 
 func setupTestDB() (*pgx.Conn, func(), error) {
-
+	os.Setenv("DB_URL", "postgres://myuser:mypassword@localhost:5432/my_database?sslmode=disable")
 	dbURL := os.Getenv("DB_URL")
+	fmt.Println("My test url", dbURL)
 
 	if dbURL == "" {
 		return nil, nil, fmt.Errorf("DB_URL is not set")
@@ -108,6 +109,70 @@ func TestCreateISA(t *testing.T) {
 			}
 			assert.WithinDuration(t, now, createdISA.CreatedAt, time.Millisecond*100)
 			assert.WithinDuration(t, now, createdISA.UpdatedAt, time.Millisecond*100)
+		})
+	}
+
+}
+
+func TestUpdate(t *testing.T) {
+	ctx := context.Background()
+	now := time.Now()
+	//Set up the test database
+	conn, cleanup, err := setupTestDB()
+	if err != nil {
+		t.Fatalf("Failed to set up database: %v", err)
+	}
+	defer cleanup()
+
+	store := postgres.NewStore(conn)
+
+	tests := map[string]struct {
+		initialISA    postgres.ISA
+		updateISA     postgres.ISA
+		expectedISA   postgres.ISA
+		errorContains string
+	}{
+		"success: Update an existing ISA": {
+			initialISA: postgres.ISA{
+				ID:               "d9e89726-46f7-4f36-99ff-c9f45fd58fb3",
+				UserID:           "6343b120-b611-4288-a8ff-9c79dec043f1",
+				FundIDs:          []string{"a8364471-0a6c-4537-a7e3-dc2a18d9f4b6"},
+				CashBalance:      10000,
+				InvestmentAmount: 25000,
+			},
+			updateISA: postgres.ISA{
+				ID:               "d9e89726-46f7-4f36-99ff-c9f45fd58fb3",
+				FundIDs:          []string{"fe865bc7-5f3e-4523-895d-67b705041e5b", "289a532f-d7c3-423a-b5cb-af4a3f26ae48"},
+				CashBalance:      15000,
+				InvestmentAmount: 35000,
+			},
+			expectedISA: postgres.ISA{
+				ID:               "d9e89726-46f7-4f36-99ff-c9f45fd58fb3",
+				UserID:           "6343b120-b611-4288-a8ff-9c79dec043f1", // UserID should remain the same
+				FundIDs:          []string{"fe865bc7-5f3e-4523-895d-67b705041e5b", "289a532f-d7c3-423a-b5cb-af4a3f26ae48"},
+				CashBalance:      15000,
+				InvestmentAmount: 35000,
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+
+			_, err := store.CreateIsa(ctx, test.initialISA)
+			require.NoError(t, err)
+
+			updatedISA, err := store.UpdateIsa(ctx, test.updateISA)
+			require.NoError(t, err)
+
+			assert.Equal(t, test.expectedISA.ID, updatedISA.ID)
+			assert.Equal(t, test.expectedISA.UserID, updatedISA.UserID)
+			assert.Equal(t, test.expectedISA.CashBalance, updatedISA.CashBalance)
+			assert.Equal(t, test.expectedISA.InvestmentAmount, updatedISA.InvestmentAmount)
+			assert.WithinDuration(t, now, updatedISA.UpdatedAt, time.Millisecond*100)
+			for i, fundID := range updatedISA.FundIDs {
+				assert.Equal(t, test.expectedISA.FundIDs[i], fundID)
+			}
 		})
 	}
 
